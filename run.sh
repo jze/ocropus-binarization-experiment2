cd195316e7cee9b605b47726748e2aafbc0da0d6
Jesper Zedlitz <jesper@zedlitz.de>
2017-04-04 14:42:23 +0200

    Tests with ocropus-nlbin and ocropus-gpageseg

--- run.sh
+++ run.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+# 
+# Run the character recognition, determine the error rate and clean up.
+# Error rate will be stored in a variable "error".
+# 
+function recognitionAndError() {
+	ocropus-rpred -q -Q2 -m fraktur.pyrnn.gz work/*.png
+	cp ground-truth/*.gt.txt work/
+	error=`ocropus-errs -e work/*.gt.txt |grep ^[0-9]`
+}
+
+function clean() {
+	rm -f work.*
+	rm -fr work/
+}
+
 function runScantailor() {
 	DPI=$1
 	THRESHOLD=$2
 	mkdir -p out
 	scantailor-cli --dpi=300 --output-dpi=$DPI --layout=1 --threshold=$THRESHOLD color.jpeg out/ && mv out/color.tif work.tif
-	ocropus-gpageseg -q --usegauss work.tif
-	ocropus-rpred -q -m fraktur.pyrnn.gz work/*.bin.png
-	cp ground-truth/*.gt.txt work/
-	echo -ne "$DPI\t$THRESHOLD\t"
-	ocropus-errs -e work/*.gt.txt |grep ^[0-9]
-	rm -fr work/
-	rm -f work.tif
-	rm -f work.pseg.png
+	ocropus-gpageseg -n -q --csminheight 100000 --usegauss work.tif
+	recognitionAndError
+	echo -e "$DPI\t$THRESHOLD\t$error" >> scantailor.csv
+	rm -fr out/
+	clean
 }
 
 function checkPrerequisites() {
@@ -27,13 +39,42 @@ function checkPrerequisites() {
 		echo "OCRopus not installed? Could not execute 'ocropus-gpageseg'. Check README.md"
 		exit 1
 	fi
+}
+
+function runNetbpm() {
+	djpeg color.jpeg | ppmtopgm |pgmnorm -wvalue 185 -bvalue 120 > work.pgm
+	ocropus-gpageseg -n -q --csminheight 100000 --usegauss work.pgm
+	recognitionAndError
+	echo -e "gpageseg bin\t$error" >> gray.csv
 	
-	exit 0
+	ocropus-nlbin work.pgm
+	ocropus-gpageseg -q -n --csminheight 100000 --usegauss work.bin.png 
+	recognitionAndError
+	echo -e "nlbin+gpageseg bin\t$error" >> gray.csv
+	
+	ocropus-gpageseg -q -n --gray --csminheight 100000 --usegauss work.nrm.png 
+	rm -f work/*.bin.png
+	recognitionAndError
+	echo -e "nlbin+gpageseg gray\t$error" >> gray.csv
+	
+	clean
 }
 
 checkPrerequisites
+
+runNetbpm
 
 runScantailor 600 0
 runScantailor 600 10
 runScantailor 600 20
 runScantailor 600 30
+runScantailor 400 0
+runScantailor 400 10
+runScantailor 400 20
+runScantailor 400 30
+runScantailor 300 0
+runScantailor 300 10
+runScantailor 300 20
+runScantailor 300 30
+
+echo "Results can be found in scantailor.csv and gray.csv"
