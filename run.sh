46e5e7d79daae7e46ebb7671a883eb3be4f8274b
Jesper Zedlitz <jesper@zedlitz.de>
2017-04-04 10:49:30 +0200

    described prerequisites

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ function runScantailor() {
 	mkdir -p out
 	scantailor-cli --dpi=300 --output-dpi=$DPI --layout=1 --threshold=$THRESHOLD color.jpeg out/ && mv out/color.tif work.tif
 	ocropus-gpageseg -q --usegauss work.tif
-	ocropus-rpred -q -m ~/sandbox/ocropy/models/fraktur.pyrnn.gz work/*.bin.png
+	ocropus-rpred -q -m fraktur.pyrnn.gz work/*.bin.png
 	cp ground-truth/*.gt.txt work/
 	echo -ne "$DPI\t$THRESHOLD\t"
 	ocropus-errs -e work/*.gt.txt |grep ^[0-9]
@@ -14,6 +14,24 @@ function runScantailor() {
 	rm -f work.tif
 	rm -f work.pseg.png
 }
+
+function checkPrerequisites() {
+	scantailor-cli -h > /dev/null 2>/dev/null
+	if [ $? != 0 ]; then
+		echo "ScanTailtor not installed? Could not execute 'scantailor-cli'. Check README.md"
+		exit 1
+	fi
+	
+	ocropus-gpageseg -h > /dev/null 2>/dev/null
+	if [ $? != 0 ]; then
+		echo "OCRopus not installed? Could not execute 'ocropus-gpageseg'. Check README.md"
+		exit 1
+	fi
+	
+	exit 0
+}
+
+checkPrerequisites
 
 runScantailor 600 0
 runScantailor 600 10
