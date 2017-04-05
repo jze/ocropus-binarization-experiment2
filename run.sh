e2d4628769b4304bfb576d5fc7beb984e0db531a
Jesper Zedlitz <jesper@zedlitz.de>
2017-04-05 18:29:31 +0200

    extended the input image

--- run.sh
+++ run.sh
@@ -30,7 +30,7 @@ function runScantailor() {
 function checkPrerequisites() {
 	scantailor-cli -h > /dev/null 2>/dev/null
 	if [ $? != 0 ]; then
-		echo "ScanTailtor not installed? Could not execute 'scantailor-cli'. Check README.md"
+		echo "ScanTailor not installed? Could not execute 'scantailor-cli'. Check README.md"
 		exit 1
 	fi
 	
@@ -48,11 +48,11 @@ function runNetbpm() {
 	echo -e "gpageseg bin\t$error" >> gray.csv
 	
 	ocropus-nlbin work.pgm
-	ocropus-gpageseg -q -n --csminheight 100000 --usegauss work.bin.png 
+	ocropus-gpageseg -q -n --threshold 0.5 --csminheight 100000 --usegauss work.bin.png 
 	recognitionAndError
 	echo -e "nlbin+gpageseg bin\t$error" >> gray.csv
 	
-	ocropus-gpageseg -q -n --gray --csminheight 100000 --usegauss work.nrm.png 
+	ocropus-gpageseg -q -n --gray --threshold 0.5 --csminheight 100000 --usegauss work.nrm.png 
 	rm -f work/*.bin.png
 	recognitionAndError
 	echo -e "nlbin+gpageseg gray\t$error" >> gray.csv
@@ -64,17 +64,20 @@ checkPrerequisites
 
 runNetbpm
 
-runScantailor 600 0
-runScantailor 600 10
-runScantailor 600 20
-runScantailor 600 30
-runScantailor 400 0
-runScantailor 400 10
-runScantailor 400 20
-runScantailor 400 30
 runScantailor 300 0
 runScantailor 300 10
 runScantailor 300 20
 runScantailor 300 30
+runScantailor 300 40
+runScantailor 400 0
+runScantailor 400 10
+runScantailor 400 20
+runScantailor 400 30
+runScantailor 400 40
+runScantailor 600 0
+runScantailor 600 10
+runScantailor 600 20
+runScantailor 600 30
+runScantailor 600 40
 
 echo "Results can be found in scantailor.csv and gray.csv"
